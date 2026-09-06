eea979dc43cb06f9728de7be3c723240eecf7521
Марат Галецкий <maratgaletski@gmail.com>
2026-09-07 02:43:10 +0300

    Добавление БД

--- bakery-server/server.js
+++ bakery-server/server.js
@@ -4,13 +4,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' })); // Увеличиваем лимит из-за картинок Base64
+app.use(express.json({ limit: '10mb' }));
 
-// Подключение к MySQL
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',      // Ваш логин от MySQL (обычно root)
-    password: '',      // Ваш пароль (в XAMPP обычно пустой)
+    user: 'root',
+    password: '',
     database: 'bakery_db',
     port: '3307'
 });
@@ -20,14 +19,12 @@ db.connect(err => {
     console.log('Подключено к MySQL!');
 });
 
-// 1. API: Получить товары (с возможностью фильтрации)
 app.get('/api/products', (req, res) => {
-    const { category } = req.query; // Ловим параметр из URL
+    const { category } = req.query;
     
     let sql = 'SELECT * FROM products';
     let params = [];
 
-    // Если запросили конкретную категорию, добавляем фильтр
     if (category) {
         sql += ' WHERE category = ?';
         params.push(category);
@@ -39,13 +36,9 @@ app.get('/api/products', (req, res) => {
     });
 });
 
- 
-// 2. API: Добавить новый товар
 app.post('/api/products', (req, res) => {
-    // Добавили category сюда:
     const { title, price, img, weight, composition, calories, energy, category } = req.body; 
     
-    // Обновили SQL-запрос (теперь 8 параметров)
     const sql = 'INSERT INTO products (title, price, img, weight, composition, calories, energy, category) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
     
     db.query(sql, [title, price, img, weight, composition, calories, energy, category], (err, result) => {
@@ -54,13 +47,11 @@ app.post('/api/products', (req, res) => {
     });
 });
 
-// 3. API: Создать новый заказ
 app.post('/api/orders', (req, res) => {
     const { clientName, clientPhone, clientAddress, clientComment, items, totalSum } = req.body;
 
     const sql = 'INSERT INTO orders (client_name, client_phone, client_address, client_comment, items, total_sum) VALUES (?, ?, ?, ?, ?, ?)';
     
-    // Превращаем объект товаров в строку JSON для хранения в БД
     const itemsJson = JSON.stringify(items);
 
     db.query(sql, [clientName, clientPhone, clientAddress, clientComment, itemsJson, totalSum], (err, result) => {
@@ -72,7 +63,6 @@ app.post('/api/orders', (req, res) => {
     });
 });
 
-// API: Удалить товар по ID
 app.delete('/api/products/:id', (req, res) => {
     const productId = req.params.id;
     const sql = 'DELETE FROM products WHERE id = ?';
@@ -86,7 +76,6 @@ app.delete('/api/products/:id', (req, res) => {
     });
 });
 
-// Запуск сервера
 app.listen(3000, () => {
     console.log('Сервер запущен на http://localhost:3000');
 });
